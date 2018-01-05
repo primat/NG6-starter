254b0fec1f8c7ad24d04762a8782a4efe15a0025
Mat Price <mathieu.price@bellmedia.ca>
2018-01-05 00:25:39 -0500

    Starter adjustments

--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
-import uiRouter from 'angular-ui-router';
+import uiRouter from '@uirouter/angularjs';
 import homeComponent from './home.component';
 
 let homeModule = angular.module('home', [
@@ -19,7 +19,7 @@ let homeModule = angular.module('home', [
 })
 
 .component('home', homeComponent)
-  
+
 .name;
 
 export default homeModule;
